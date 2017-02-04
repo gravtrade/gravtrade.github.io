1f2fcaecaedae8d8c13500143b62e13b9e158fd8
gravtrade <gravtrader@gmail.com>
2017-02-04 09:36:27 +0800

    Update currency-exchange.php

--- currency-exchange.php
+++ currency-exchange.php
@@ -1,10 +1,8 @@
-$json = file_get_contents('http://apilayer.net/api/live?access_key=6569f27ce6a4c309c0027288f3ded2c6&currencies=USD,AUD,CAD,PLN,MXN&format=1');
-
-$array = json_decode($json);
-
-$quotes=array();
-foreach ($array as $value) { 
-    $quotes[]=$value->quotes;
-}
-
-print_r($quotes);
+<?php 
+$json_url = "http://apilayer.net/api/live?access_key=6569f27ce6a4c309c0027288f3ded2c6&currencies=USD,AUD,CAD,PLN,MXN&format=1";
+$json = file_get_contents($quotes);
+$data = json_decode($json, TRUE);
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+?>
