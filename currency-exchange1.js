13b8d0ae6b3fecd35807abb6ed32c7cd47dc8209
gravtrade <gravtrader@gmail.com>
2017-02-04 12:55:29 +0800

    Update currency-exchange1.js

--- currency-exchange1.js
+++ currency-exchange1.js
@@ -1,5 +1,26 @@
-function load() {
-	var mydata = JSON.parse(data);
-	alert(mydata[0].source);
-	alert(mydata[0].quotes);
+function loadJSON(callback) {
+
+var xobj = new XMLHttpRequest();
+xobj.overrideMimeType("application/json");
+xobj.open('GET', 'http://apilayer.net/api/live?access_key=6569f27ce6a4c309c0027288f3ded2c6&currencies=USD,AUD,CAD,PLN,MXN&format=1', true);
+xobj.onreadystatechange = function () {
+if (xobj.readyState == 4 && xobj.status == "200") {
+
+// .open will NOT return a value but simply returns undefined in async mode so use a callback
+callback(xobj.responseText);
+
 }
+}
+xobj.send(null);
+
+}
+
+// Call to function with anonymous callback
+loadJSON(function(response) {
+// Do Something with the response e.g.
+jsonresponse = JSON.parse(response);
+
+// Assuming json data is wrapped in square brackets as Drew suggests
+console.log(jsonresponse[0].quotes);
+
+});
