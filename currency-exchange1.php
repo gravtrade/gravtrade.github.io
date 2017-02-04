568ef13b245a2c2875e97ab3f824dfcd2a22773c
gravtrade <gravtrader@gmail.com>
2017-02-04 12:48:51 +0800

    Update and rename currency-exchange1.php to currency-exchange1.js

--- currency-exchange1.php
+++
@@ -1,4 +0,0 @@
-<?php
-$myJSON = file_get_contents('http://apilayer.net/api/live?access_key=6569f27ce6a4c309c0027288f3ded2c6&currencies=USD,AUD,CAD,PLN,MXN&format=1');
-echo "myFunc(".$myJSON.");";
-?>
