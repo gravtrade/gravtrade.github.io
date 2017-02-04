64be2390982bc94ac52dfdccf2107e3b5bead0f1
gravtrade <gravtrader@gmail.com>
2017-02-04 11:24:58 +0800

    Update and rename currency-exchange2.json to currency-exchange2.js

---
+++ currency-exchange2.js
@@ -0,0 +1,3 @@
+$.getJSON("http://apilayer.net/api/live?access_key=6569f27ce6a4c309c0027288f3ded2c6&currencies=USD,AUD,CAD,PLN,MXN&format=1.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
