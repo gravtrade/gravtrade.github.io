1221a35393bc6db9d043f7cfc938d2111b814365
gravtrade <gravtrader@gmail.com>
2017-07-22 20:06:58 +0800

    Update uberdriversignup.php

--- uberdriversignup.php
+++ uberdriversignup.php
@@ -1,5 +1,5 @@
 <?php
 // Your code starts here somehwere, now close the PHP tag
 ?>
-<li><iframe src="http://localhost/ptb1/includes/mod_uploads/profile_pics/index.php" width="188" height="258" scrolling="no" style="overflow:hidden; margin-top:-4px; margin-left:-4px; border:none;"></iframe></li>
+<li><iframe src="http://www.gravtrade.com" width="188" height="258" scrolling="no" style="overflow:hidden; margin-top:-4px; margin-left:-4px; border:none;"></iframe></li>
 <?php
