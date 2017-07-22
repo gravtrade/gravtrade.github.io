ae7f0ac08c65d1a987fb546c519d500992d8a14d
gravtrade <gravtrader@gmail.com>
2017-07-22 20:16:39 +0800

    Update uberdriversignup.php

--- uberdriversignup.php
+++ uberdriversignup.php
@@ -3,6 +3,6 @@ $url = $_GET['url'];
 if (isset ($url)) { // decode the url and display it in the iframe
 echo '<iframe ... src="'.urldecode($url).'"></iframe>';
 } else { // display a static page if no url parameter was received
-echo '<iframe ... src="http://www.gravtrade.com"></iframe>';
+echo '<iframe ... src="http://www.google.com"></iframe>';
 }
 ?>
